0585b5005dacaa7c1682e3c4686ce3df11e417fd
maycom306 <maycomcachina306@gmail.com>
2026-07-25 11:17:40 -0300

    criação do handler para separar as rotas

--- router/rotas.go
+++ router/rotas.go
@@ -1,37 +1,22 @@
 package router
 
 import (
-	"net/http"
+	
 	"github.com/gin-gonic/gin"
+	"github.com/maycom306/aprender_go_opportunites/handler"
 )
 
 func Incializarotas(router *gin.Engine){
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func (c *gin.Context){
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "GET Opening", 
-			})
-		})
-				v1.POST("/opening", func (c *gin.Context){
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "POST Opening", 
-			})
-		})
-				v1.DELETE("/opening", func (c *gin.Context){
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "DELETE Opening", 
-			})
-		})
-				v1.PUT("/opening", func (c *gin.Context){ //PUT é igual a update 
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "PUT Opening", 
-			})
-		})
-				v1.GET("/openings", func (c *gin.Context){
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "GET Openings", 
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+
+		v1.POST("/opening", handler.CreateOpeningHandler)
+
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+
+		v1.PUT("/opening", handler.UpdateOpeningHandler) //UT é igual a update 
+
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
